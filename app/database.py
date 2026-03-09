76c1365d323d954f7457b024cbd32297a2e6a435
Amrita Hariharan <amrita.hariharan2006@gmail.com>
2026-03-10 03:32:11 +0530

    connect cloud database

--- app/database.py
+++ app/database.py
@@ -4,6 +4,15 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 # Load .env file from project root
 env_path = Path(__file__).parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
