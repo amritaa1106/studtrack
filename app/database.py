7e32cde80b7048ffe8081b38f9cf3ab904ba2a94
Amrita Hariharan <amrita.hariharan2006@gmail.com>
2026-04-13 07:24:16 +0530

    fix: switch to sqlite fully

--- app/database.py
+++ app/database.py
@@ -26,7 +26,7 @@ Base = declarative_base()
 env_path = Path(__file__).parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set. Please check your .env file.")
